6abc88f030735a7a983bb1b3f14684e641a2fb20
Anna Tsolakou <tsolakou.a@gmail.com>
2019-11-19 16:07:38 +0100

    include airport search

--- amadeus_demo_api/demo/urls.py
+++ amadeus_demo_api/demo/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.demo, name='demo_form'),
+    path('origin_airport_search/', views.origin_airport_search, name='origin_airport_search'),
+    path('destination_airport_search/', views.destination_airport_search, name='destination_airport_search'),
 ]
